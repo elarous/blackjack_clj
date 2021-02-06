bc6d047a07fa502fb3e0aa0d040d599357467f7a
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2021-02-06 17:48:16 +0100

    define the data model

--- src/blackjack_clj/core.clj
+++ src/blackjack_clj/core.clj
@@ -1,1 +1,18 @@
 (ns blackjack-clj.core)
+
+;; Define the data model
+(def initial-state
+  {:score       {:wins 0 :losses 0}
+   :cards       (shuffle (for [type [:hearts :clubs :diamonds :spades]
+                               number [\A 2 3 4 5 6 7 8 9 \J \Q \K]]
+                           (hash-map :type type :number number)))
+   :round       1
+   :turn        :dealer
+   :player-hand []
+   :dealer-hand []})
+
+
+(comment
+ initial-state
+
+  ,)
